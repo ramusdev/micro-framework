858ed1ddbb46f3317517ce2faa7b46b034f075f1
Roman <ram219219@gmail.com>
2018-08-15 12:33:32 +0300

    Add ER diagram

--- app/config.php
+++ app/config.php
@@ -9,13 +9,16 @@
 ini_set( 'display_errors', 1 );
 
 // Database name
-define( 'DB_NAME', 'mf' );
+//define( 'DB_NAME', 'mf' );
+define( 'DB_NAME', 'a0139772_bwt' );
 
 // Database password
-define( 'DB_PASSWORD', 'mysql' );
+//define( 'DB_PASSWORD', 'mysql' );
+define( 'DB_PASSWORD', 'A9VHH94T' );
 
 // Database username
-define( 'DB_USER', 'mysql' );
+//define( 'DB_USER', 'mysql' );
+define( 'DB_USER', 'a0139772_bwt' );
 
 // Database hosname
 define( 'DB_HOST', 'localhost' );
